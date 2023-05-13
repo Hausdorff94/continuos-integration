dc0ebe4a42ef87018f611a98ba98ead2da9e7cee
Johny Vallejo-Sánchez <35435216+Hausdorff94@users.noreply.github.com>
2023-05-13 01:07:22 +0000

    Add basic_operations script

---
+++ basic_operations.py
@@ -0,0 +1,54 @@
+def mysum(a: int,b: int) -> int:
+    '''
+    This function returns the sum of two numbers
+
+    Parameters:
+    a: int
+    b: int
+
+    Returns:
+    int
+    '''
+    return a+b
+
+def sub(a:int, b:int) -> int:
+    '''
+    This function returns the difference of two numbers
+    
+    Parameters:
+    a: int
+    b: int
+
+    Returns:
+    int
+    '''
+    return a-b
+
+def mul(a:int,b:int) -> int:
+    '''
+    This function returns the product of two numbers
+
+    Parameters:
+    a: int
+    b: int
+
+    Returns:
+    int
+    '''
+    return a*b
+
+def div(a: int,b: int) -> float:
+    '''
+    This function returns the division of two numbers
+
+    Parameters:
+    a: int
+    b: int
+
+    Returns:
+    float
+    '''
+    if b==0:
+        return "Error: Division by zero"
+
+    return a/b
